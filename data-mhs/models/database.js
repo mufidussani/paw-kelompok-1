5e1d689488c2a7c4c8da2a9e28a311dfc5d1204b
mufidussani <sanimufidus@gmail.com>
2021-11-22 22:47:43 +0700

    memperbaiki struktur file

--- data-mhs/models/database.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://admin:admin@datamahasiswa.rhqba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser:true});
-
-require('./mahasiswa.model');
