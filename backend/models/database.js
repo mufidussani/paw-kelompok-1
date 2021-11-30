2a5ad42af9630cc58a62a73b290b8bb5e4fbff79
mufidussani <sanimufidus@gmail.com>
2021-11-30 12:53:01 +0700

    memperbaiki connection string database

--- backend/models/database.js
+++ backend/models/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://admin:admin@datamahasiswa.rhqba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin:nimda@storium.rhqba.mongodb.net/PAW?retryWrites=true&w=majority",{useNewUrlParser:true});
 
 require('./mahasiswa.model');
